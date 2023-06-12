b1e6956dd8d8e47155ef8737964922d15f270995
Anshul Kashyap <cyb3rblaze@gmail.com>
2023-06-12 06:54:21 -0700

    Updates

--- evaluation_frameworks/segment_prediction_dataset.py
+++ evaluation_frameworks/segment_prediction_dataset.py
@@ -5,7 +5,7 @@ from torch.utils.data import Dataset
 from tqdm import tqdm
 
 import scipy.io
-from scipy.signal import butter, sosfilt, freqs
+from scipy.signal import butter, sosfilt, resample
 
 import matplotlib.pyplot as plt
 
@@ -26,6 +26,12 @@ class CustomDataset(Dataset):
 
         all_subject_brain_data = None
 
+        # for resampling purposes
+        bundle = torchaudio.pipelines.WAV2VEC2_XLSR53
+
+        self.audio_sample_rate = 16000
+        self.brain_data_sample_rate = 150
+
         # 49 subjects
         for i in tqdm(range(1, num_subjects + 1)):
             if i not in exclude:
@@ -41,7 +47,9 @@ class CustomDataset(Dataset):
                 for channel in raw_data:
                     final_filtered = sosfilt(sos, channel)
 
-                    final_filtered = final_filtered - np.mean(final_filtered[:int(500*0.5)])
+                    final_filtered = resample(final_filtered, int(len(final_filtered)*(self.brain_data_sample_rate/500)))
+
+                    final_filtered = final_filtered - np.mean(final_filtered[:int(self.brain_data_sample_rate*0.5)])
                     final_filtered = (final_filtered - np.mean(final_filtered)) / np.std(final_filtered)
                     final_filtered = np.clip(final_filtered, -20, 20)
 
@@ -58,20 +66,12 @@ class CustomDataset(Dataset):
 
         num_subjects = num_subjects - len(exclude)
 
-        # for resampling purposes
-        bundle = torchaudio.pipelines.WAV2VEC2_ASR_BASE_960H
-
-        self.audio_sample_rate = bundle.sample_rate
-        self.brain_data_sample_rate = 500
-
         # audio file parsing
         all_audio_data = None
 
         for _, file in enumerate(os.listdir(data_dir + "/audio")):
             waveform, sample_rate = torchaudio.load(data_dir + "/audio/" + file)
-
-            if sample_rate != bundle.sample_rate:
-                waveform = torchaudio.functional.resample(waveform, sample_rate, bundle.sample_rate)
+            waveform = torchaudio.functional.resample(waveform, sample_rate, self.audio_sample_rate)
             
             if _ == 0:
                 all_audio_data = waveform
@@ -82,9 +82,9 @@ class CustomDataset(Dataset):
 
         all_audio_data = all_audio_data[0][:all_audio_data.shape[1]//(self.audio_sample_rate*3)*(self.audio_sample_rate*3)]
 
-        all_audio_data = all_audio_data.view(-1, self.audio_sample_rate*3).repeat(num_subjects, 1, 1).reshape((-1, 48000))
+        all_audio_data = all_audio_data.view(-1, self.audio_sample_rate*3).repeat(num_subjects, 1, 1).reshape((-1, 16000))
 
-        self.all_subject_brain_data = torch.sum(all_subject_brain_data[:, :, :all_subject_brain_data.shape[-1]//1500*1500].view(num_subjects, all_subject_brain_data.shape[1], -1, 150, 10), dim=4)[:, :, :, :T_out]
+        self.all_subject_brain_data = torch.mean(all_subject_brain_data[:, :, :all_subject_brain_data.shape[-1]//450*450].view(num_subjects, all_subject_brain_data.shape[1], -1, 50, 9), dim=4)[:, :, :, :T_out]
 
         self.num_samples = min(self.all_subject_brain_data.shape[2] * self.all_subject_brain_data.shape[0], all_audio_data.shape[0])
 
